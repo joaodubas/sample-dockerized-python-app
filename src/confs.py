3976e062bb9f53e09e5c95f76ed3386d57251c34
João Paulo Dubas <joao.dubas@gmail.com>
2014-06-23 13:22:24 -0300

    [add] Project configuration to db access.

--- src/confs.py
+++ src/confs.py
@@ -1,5 +1,13 @@
 # encoding: utf-8
+import os
 
 
 class Configuration(object):
-    pass
+    DATABASE = {
+        'name': os.getenv('DB_NAME'),
+        'engine': 'peewee.PostgresqlDatabase',
+        'user': os.getenv('DB_USER'),
+        'passwd': os.getenv('DB_PASS'),
+    }
+    DEBUG = bool(os.getenv('APP_DEBUG'))
+    SECRET_KEY = os.getenv('APP_SECRET')
